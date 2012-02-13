90c6ef4fc99fef8f7fcd28dc43afbb6e2d6e111e
Sveinung Dalatun <sdalatun@gmail.com>
2012-02-13 22:39:41 +0100

    Added a module that functions as a smoke-test for the maven-plugin.

---
+++ neworder-maven-plugin-smoketests/src/test/java/no/neworder/AppTest.java
@@ -0,0 +1,13 @@
+package no.neworder;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void should_test_a() {
+        //  Passing test
+    }
+}
